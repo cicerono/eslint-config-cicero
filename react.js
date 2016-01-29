f5d9023180bf55f96dcaf996a411a7a9c79ebc4b
Rolf Erik Lekang <me@rolflekang.com>
2016-01-29 07:53:14 +0100

    Disable react/no-multi-comp

--- react.js
+++ react.js
@@ -25,7 +25,7 @@ module.exports = {
     "react/jsx-no-undef": 2,
     "react/jsx-uses-react": 2,
     "react/no-deprecated": 1,
-    "react/no-multi-comp": 2,
+    "react/no-multi-comp": 0,
     "react/no-unknown-property": 2,
     "react/prop-types": 2,
     "react/sort-comp": [0, {"order": order}],
