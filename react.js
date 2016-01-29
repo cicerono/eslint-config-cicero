f6b374bc0097450391d93a775e39d80f4243e774
Rolf Erik Lekang <me@rolflekang.com>
2016-01-29 07:52:54 +0100

    Add react/no-deprecated

--- react.js
+++ react.js
@@ -24,6 +24,7 @@ module.exports = {
     "react/jsx-boolean-value": [2, "always"],
     "react/jsx-no-undef": 2,
     "react/jsx-uses-react": 2,
+    "react/no-deprecated": 1,
     "react/no-multi-comp": 2,
     "react/no-unknown-property": 2,
     "react/prop-types": 2,
