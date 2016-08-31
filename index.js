d7a76c72dafb32c366dd1d2009f36e995da4cc4d
Rolf Erik Lekang <me@rolflekang.com>
2016-08-31 14:12:34 +0200

    fix: Update flowtype rules

--- index.js
+++ index.js
@@ -43,11 +43,14 @@ module.exports = {
     }],
 
     "flowtype/define-flow-type": 1,
-    "flowtype/require-parameter-type": 1,
-    "flowtype/require-return-type": [1, "always", {"annotateUndefined": "never"}],
+    "flowtype/require-valid-file-annotation": 1,
     "flowtype/space-after-type-colon": [1, "always"],
     "flowtype/space-before-type-colon": [1, "never"],
+    "flowtype/space-before-generic-bracket": [1, "never"],
+    "flowtype/union-intersection-spacing": [1, "always"],
+    "flowtype/generic-spacing": [1, "never"],
     "flowtype/type-id-match": [1, "^([A-Z][a-z0-9]+)+"],
+    "flowtype/valid-syntax": 2,
     "flowtype/use-flow-type": 1,
 
     "arrow-body-style": 0,
