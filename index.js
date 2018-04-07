a672ba4605c34ae369ceb583854d5c30ec198350
Rolf Erik Lekang <me@rolflekang.com>
2018-04-07 21:22:37 +0200

    fix: Remove lodash config from index and react
    It has already been extracted into lodash.js

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 /* eslint-disable quote-props */
 module.exports = {
-  extends: [
-    "eslint-config-airbnb-base",
-    "plugin:lodash/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-  ],
-  plugins: ["lodash", "import"],
+  extends: ["eslint-config-airbnb-base", "plugin:import/errors", "plugin:import/warnings"],
+  plugins: ["import"],
   rules: {
-    "lodash/prefer-lodash-method": [2, {ignoreObjects: ["wrapper", "component"]}],
-    "lodash/import-scope": [2, "member"],
     "max-len": [2, 100, 2, {ignorePattern: "(^import)|(^} from)"}],
     "no-multiple-empty-lines": [2, {max: 1, maxBOF: 0, maxEOF: 0}],
     "object-curly-spacing": [2, "never"],
