a71e7fcbaad207a810093a2a70b37f8fd937be61
Chris Searle <chris@chrissearle.org>
2018-05-22 09:24:30 +0200

    fix: ignore isArray in lodash rules
    Array.isArray is supported for IE9 and above and works better with
    flow refinement for some versions.

--- lodash.js
+++ lodash.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ["plugin:lodash/recommended"],
   plugins: ["lodash"],
   rules: {
-    "lodash/prefer-lodash-method": [2, {ignoreObjects: ["wrapper", "component"]}],
+    "lodash/prefer-lodash-method": [2, {ignoreObjects: ["wrapper", "component"], ignoreMethods: ["isArray"]}],
     "lodash/import-scope": 0,
   },
 };
